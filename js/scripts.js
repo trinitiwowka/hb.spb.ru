3480d5bd2e73e8eea568c61833699c847ac6c2e4
art-kosheen <kashirin1242@gmail.com>
2016-12-02 00:24:55 +0200

    обработка события кнопки на смс в scripts.js строка - 131.

--- js/scripts.js
+++ js/scripts.js
@@ -120,6 +120,16 @@ $(document).ready(function() {
 					}
 				});
 
+				console.log('price');
+
+				thxPrice();
+
+			} else if($(this).data('name')=='price-phone') {
+
+				console.log('price-phone');
+
+				// сдесь код для кнопки на смс
+
 				thxPrice();
 			} else {
 
@@ -348,5 +358,19 @@ function checkForm(form1) {
 $(document).ready(function() {
 
     $('input[name="phone"]').mask("+7 (999) 999-9999");
+
+	$('.tabs__control-link').on('click', function (e) {
+		e.preventDefault();
+
+		var item = $(this).closest('.tabs__controls-item'),
+			contentItem = $('.tabs__item'),
+			itemPosition = item.index();
+
+		contentItem.eq(itemPosition)
+			.add(item)
+			.addClass('active')
+			.siblings()
+			.removeClass('active');
+	});
                  
 });
